2475c747414fbee0aeae07e000505a94d51b3005
paproc <pajainf@seznam.cz>
2022-12-03 16:54:47 +0100

    start.sh

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,20 +1,16 @@
+cd /var/www/fykos-webs
 if [ ! -f /var/log/settup.log ]
 then
     chmod -R 777 /var/log/
     echo "instaling runtime" >> /var/log/settup.log
-    cd /var/www/fykos-webs
     chmod -R 777 .
     composer install
-    npm install --dev
-    npm run build &
-    git config core.fileMode false
+    npm install --include=dev
     cp app/config/config.local.neon.example app/config/config.fof.local.neon
     cp app/config/config.local.neon.example app/config/config.fol.local.neon
     cp app/config/config.local.neon.example app/config/config.dsef.local.neon
     cp app/config/config.local.neon.example app/config/config.fykos.local.neon
     cp app/config/config.local.neon.example app/config/config.vyfuk.local.neon
 fi
-cd /var/www/fykos-webs
 service apache2 start
-tail -f /var/www/fykos-webs/log/.gitignore &
 npm run dev
