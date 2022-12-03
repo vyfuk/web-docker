d0cc86b84cef3673409edf41b839eb2743284f88
paproc <pajainf@seznam.cz>
2022-12-03 17:35:29 +0100

    log

--- .docker/start.sh
+++ .docker/start.sh
@@ -2,10 +2,11 @@ cd /var/www/fykos-webs
 if [ ! -f /var/log/settup.log ]
 then
     chmod -R 777 /var/log/
+    mkdir /var/log/apache2/
     echo "instaling runtime" >> /var/log/settup.log
     chmod -R 777 .
-    composer install
-    npm install --include=dev
+    composer install  >> /var/log/settup.log
+    npm install --include=dev  >> /var/log/settup.log
     cp app/config/config.local.neon.example app/config/config.fof.local.neon
     cp app/config/config.local.neon.example app/config/config.fol.local.neon
     cp app/config/config.local.neon.example app/config/config.dsef.local.neon
